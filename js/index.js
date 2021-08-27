1550567179e0df9747899a23e4d4608c4b7d5327
ghaithdiab <ghaith.diab@hotmail.com>
2021-08-27 10:29:45 +0200

    implement an interface

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// const dropdow=document.querySelector(".btn-ingredient");
+// const menu=document.querySelector(".dropdown-menu");
+
+// dropdow.addEventListener("click",()=>{
+//   menu.style.top="-55 !important";
+// });
