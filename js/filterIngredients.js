2c3243faea3d51739846ef484c9fd4115cb0039c
ghaithdiab <ghaith.diab@hotmail.com>
2021-09-09 08:36:50 +0200

    filter ingredients

---
+++ js/filterIngredients.js
@@ -0,0 +1,25 @@
+import recipes from "./recipes.js";
+
+const filterIngredients =()=>{
+  /* array contain all ingredients from recipes */
+  const ingredients=new Array();
+  /* array contain uniqe value of ingredients  (value with out reapt) */
+  const uniqeIngredients=new Array();
+  /* put all ingredients for each recipe in our array(ingredients) */
+  recipes.forEach(element=>{
+    const allIngredients=element.ingredients;
+    allIngredients.forEach(ele=>{
+      ingredients.push((ele.ingredient).toLowerCase());
+    });
+  });
+/* filter ingredients and push to array uniqeIngredients */
+  ingredients.forEach(ele=>{
+    if(!uniqeIngredients.includes(ele)){
+      uniqeIngredients.push(ele);
+    }
+  });
+
+  return uniqeIngredients;
+}
+
+export default filterIngredients;
--- js/index.js
+++ js/index.js
@@ -1,15 +1,17 @@
 import recipes from "./recipes.js";
+import filterIngredients from "./filterIngredients.js";
 import functionalityDropdownMenu from "./functionalityDropdownMenu.js";
 
 
 functionalityDropdownMenu();
 
-// const ingredientsDiv=document.querySelector('.ingredient');
-// const uniqeIngredients=filterIngredients();
-// uniqeIngredients.forEach(element=>{
-//   const createLink=document.createElement("a");
-//   createLink.setAttribute("class","dropdown-item");
-//   createLink.setAttribute("href","#");
-//   createLink.innerText=element;
-//   ingredientsDiv.appendChild(createLink);
-// });
+const ingredientsDiv=document.querySelector('.ingredient');
+const uniqeIngredients=filterIngredients();
+/* creat link for each ingredient and append it to dropdown menu*/
+uniqeIngredients.forEach(element=>{
+  const createLink=document.createElement("a");
+  createLink.setAttribute("class","dropdown-item");
+  createLink.setAttribute("href","#");
+  createLink.innerText=element;
+  ingredientsDiv.appendChild(createLink);
+});
